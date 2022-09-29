2742a2ddd208d2a78e12f79b81490a0196f919cd
Seyni Moumouni <seynimoumouni@outlook.fr>
2022-09-29 17:08:28 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+const { version } = require("chai")
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-gas-reporter")
+require("hardhat-deploy")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmation: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.0" }, { version: "0.8.8" }],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        noColors: true,
+        outputFile: "gas-report.txt",
+    },
+}
